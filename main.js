becc92cdced1d551d687103887a68fbc191c17fb
karinamachado <karinamachado@MacBook-Pro-de-Karina.local>
2020-08-17 16:11:07 -0300

    magic ball

---
+++ main.js
@@ -0,0 +1,55 @@
+let userName = '';
+
+userName ? console.log(`Hello ${userName}`) : console.log('Hello');
+
+
+const userQuestion = "Will I become a werewolf tonight?";
+console.log(userQuestion); 
+
+
+const randomNumber = Math.floor(Math.random() * 8);
+// console.log(randomNumber);
+
+let eightBall = '';
+
+
+switch(randomNumber){
+  case 0:
+  eightBall = 'It is certain';
+  break;
+
+  case 1:
+  eightBall = 'Its is decidedly so';
+  break;
+
+  case 2:
+  eightBall = 'Reply hazy try again';
+  break;
+
+  case 3:
+  eightBall = 'Cannot predict now';
+  break;
+
+  case 4:
+  eightBall = 'Do not count on it';
+  break;
+
+  case 5:
+  eightBall = 'My sources say no';
+  break;
+
+  case 6:
+  eightBall = 'Oulook not so good';
+  break;
+
+  case 7:
+  eightBall = 'Signs points to yes';
+  break;
+
+  case 8:
+  eightBall = 'another'
+  break;
+
+}
+
+console.log(`The eight ball answered: ${eightBall}`);
